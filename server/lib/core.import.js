413620bf94cd6c84a0d128e1ec61e9606f293b46
Nicolas Iglesias <nico@webpolis.com.ar>
2015-12-26 15:45:56 -0300

    secure repositories

--- server/lib/core.import.js
+++ server/lib/core.import.js
@@ -1,6 +1,10 @@
 import * as repositories from '../../lib/repositories/index';
+import {default as secureRepositories} from './security';
 
 export default function init() {
   // publish
   Meteor.publish('assets.list', () => repositories.assets.find({}));
+
+  // secure repositories
+  secureRepositories();
 }
--- server/lib/security.import.js
+++ server/lib/security.import.js
@@ -2,5 +2,15 @@ import * as repositories from '../../lib/repositories/index';
 
 export default function init() {
   // secure repositories
-  
+  repositories.assets.deny({
+    update: function(userId, doc, fields, modifier) {
+      return true;
+    },
+    insert: function(userId, doc) {
+      return true;
+    },
+    remove: function(userId, doc) {
+      return true;
+    }
+  })
 }
