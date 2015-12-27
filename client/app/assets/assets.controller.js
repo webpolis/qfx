3854b4a583a0064dd9ea226048507c001c7f92e8
Nicolas Iglesias <nico@webpolis.com.ar>
2015-12-27 13:48:22 -0300

    improve search (by properties)

--- client/app/assets/assets.controller.js
+++ client/app/assets/assets.controller.js
@@ -3,7 +3,7 @@ angular
   .controller('assetsCtl', assetsCtl);
 
 function assetsCtl($scope, $reactive, assetsSvc) {
-  const vm = this;
+  let vm = this;
   let reactiveCtx = $reactive(vm).attach($scope);
 
   vm.helpers({
@@ -12,9 +12,20 @@ function assetsCtl($scope, $reactive, assetsSvc) {
     }
   })
 
+  vm.search = search;
+
   init();
 
   function init() {
     assetsSvc.init(reactiveCtx);
+  }
+
+  function search(value, index, array) {
+    if (typeof reactiveCtx.scope.query !== 'undefined' && reactiveCtx.scope.query.length > 0) {
+      var re = new RegExp(reactiveCtx.scope.query, 'gi');
+      return (re.test(value.name) || re.test(value.ticker));
+    }
+
+    return true;
   }
 }
