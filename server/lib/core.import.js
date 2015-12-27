b4efe8e440b72ce15043b41091fcd05d208544f0
Nicolas Iglesias <nico@webpolis.com.ar>
2015-12-27 00:21:47 -0300

    gral fixes

--- server/lib/core.import.js
+++ server/lib/core.import.js
@@ -1,9 +1,24 @@
 import * as repositories from '../../lib/repositories/index';
-import {default as secureRepositories} from './security';
+import {
+  default as secureRepositories
+}
+from './security';
+import {
+  default as etoroImporter
+}
+from './importers/etoro';
 
 export default function init() {
   // publish
   Meteor.publish('assets.list', () => repositories.assets.find({}));
+
+  // import
+  let etoroInstruments = new etoroImporter({
+    ticker: 'SymbolFull',
+    name: 'InstrumentDisplayName',
+    icon: 'Images[0].Uri',
+    type: 'ExchangeID'
+  });
 
   // secure repositories
   secureRepositories();
