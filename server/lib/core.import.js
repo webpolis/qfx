420b3c6e5ca72dfd55641d5f0216474d4c4c7e73
Nicolas Iglesias <nico@webpolis.com.ar>
2015-12-27 13:20:02 -0300

    formatting

--- server/lib/core.import.js
+++ server/lib/core.import.js
@@ -14,19 +14,21 @@ export default function init() {
     active: true
   }));
 
-  // import assets
-  let etoroInstruments = new etoroImporter({
-    ticker: 'SymbolFull',
-    name: 'InstrumentDisplayName',
-    icon: '@Images[0].Uri',
-    type: 'ExchangeID',
-    ref: 'InstrumentID'
-  }, 'https://api.etorostatic.com/sapi/instrumentsmetadata/V1.1/instruments');
+  if (repositories.assets.find().count() === 0) {
+    // import assets
+    let etoroInstruments = new etoroImporter({
+      ticker: 'SymbolFull',
+      name: 'InstrumentDisplayName',
+      icon: '@Images[0].Uri',
+      type: 'ExchangeID',
+      ref: 'InstrumentID'
+    }, 'https://api.etorostatic.com/sapi/instrumentsmetadata/V1.1/instruments');
 
-  for (let ei of etoroInstruments) {
-    try {
-      repositories.assets.insert(ei);
-    } catch (err) {}
+    for (let ei of etoroInstruments) {
+      try {
+        repositories.assets.insert(ei);
+      } catch (err) {}
+    }
   }
 
   // secure repositories
