e30a78b7962a47274cfa596614ca43a03f76b32e
Nicolas Iglesias <nico@webpolis.com.ar>
2015-12-29 02:18:21 -0300

    add heat directive

--- client/app/statistics/statistics.component.js
+++ client/app/statistics/statistics.component.js
@@ -1,6 +1,7 @@
 angular
   .module('app.statistics.components', ['app', 'app.statistics.services'])
-  .directive('statistics', statisticsDtv);
+  .directive('statistics', statisticsDtv)
+  .directive('heat', statisticsHeatDtv);
 
 function statisticsDtv() {
   return {
@@ -15,7 +16,29 @@ function statisticsDtv() {
   }
 }
 
-function statisticsCtl($scope, $reactive, statisticsSvc) {
+function statisticsHeatDtv() {
+  return {
+    restrict: 'E',
+    template: '<span class="heat-box">&nbsp;</span><span class="heat-box">&nbsp;</span><span class="heat-box">&nbsp;</span><span class="heat-box">&nbsp;</span><span class="heat-box">&nbsp;</span>',
+    scope: {
+      stat: '='
+    },
+    link: function link($scope, $element, $attrs) {
+      $('.heat-box', $element).heatcolor(() => scale($scope.stat.avg, [$scope.stat.min, $scope.stat.max], [1, 100]), {
+        maxval: 100,
+        minval: 1,
+        colorStyle: 'greentored',
+        lightness: 0
+      });
+
+      function scale(value, r1, r2) {
+        return (value - r1[0]) * (r2[1] - r2[0]) / (r1[1] - r1[0]) + r2[0];
+      }
+    }
+  }
+}
+
+function statisticsCtl($scope, $reactive, $timeout, statisticsSvc) {
   let vm = this;
   let reactiveCtx = $reactive(vm).attach($scope);
 
